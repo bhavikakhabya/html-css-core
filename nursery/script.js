63f51fdb8f40cde4e036e66df1fad800da1f88f0
Bhavika Khabya <bhavikakhabya@Bhavikas-MacBook-Air.local>
2026-02-23 21:52:47 +0530

    HTML_CSS-ASSIGNMENTS

---
+++ nursery/script.js
@@ -0,0 +1,27 @@
+const filterButtons = document.querySelectorAll(".filter-btn");
+const plantCards = document.querySelectorAll(".plant-card");
+
+filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        filterButtons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+
+        const filter = button.dataset.filter;
+
+        plantCards.forEach(card => {
+            if (filter === "all" || card.dataset.category.includes(filter)) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    });
+});
+
+const helloBtn = document.getElementById("helloBtn");
+const helloMsg = document.getElementById("helloMsg");
+
+helloBtn.addEventListener("click", () => {
+    helloMsg.textContent = "🌿 Thanks for visiting GreenNest! We can’t wait to see you.";
+});
